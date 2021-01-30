adb5b9f9807d52314d7e49acfaa897a98a59f217
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-01-30 22:38:13 +0700

    map

---
+++ collection/map/capital.dart
@@ -0,0 +1,11 @@
+main() {
+  var capital = {'Jakarta': 'Indonesia', 'London': 'England', 'Tokyo': 'Japan'};
+
+  print(capital['Jakarta']);
+
+  var mapKeys = capital.keys;
+  print(mapKeys);
+  print(capital.values);
+  capital['New Delhi'] = 'India';
+  print(capital);
+}
