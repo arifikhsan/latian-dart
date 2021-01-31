d86cb158458c13809bb2616b8b62610625b3818f
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-01-31 18:50:24 +0700

    closure

---
+++ functions/closure.dart
@@ -0,0 +1,12 @@
+main() {
+  var closureExample = calculate(2);
+
+  closureExample();
+  closureExample();
+}
+
+Function calculate(base) {
+  var count = 1;
+
+  return () => print("Value is ${base + count++}");
+}
